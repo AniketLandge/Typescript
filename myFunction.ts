bf97b1b3f44b1c94a299508a4261836e430d41fb
AniketLandge <aniketlandge8896@gmail.com>
2023-03-19 10:15:03 +0530

    added more scenarios

--- myFunction.ts
+++ myFunction.ts
@@ -1,4 +1,4 @@
-const addTwo = (num: number) => { return num + 2 }
+const addTwo = (num: number): number => { return num + 2 } //function will always return number value
 addTwo(5);
 // addTwo("3") this line will throw an error
 
@@ -11,3 +11,15 @@ signupUser("rohan","rohan.dev",false);
 
 const loginUser = (name:string, email:string, isPaid:boolean = false) => {  }
 loginUser("ro", "ro@r.com")
+
+const heros = ["spiderman", "ironman", "thor"];
+heros.map((hero):string => {
+    return `current hero is ${hero}`
+})
+
+function consoleError( errMsg:string):void {
+    console.log(errMsg);
+}
+function handleError( errMsg:string): never {
+    throw new Error(errMsg);
+}
